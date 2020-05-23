3961b76dc0dbb78a589e28ecf5f7f4511b4de45f
technicalwalachannel <65289959+technicalwalachannel@users.noreply.github.com>
2020-05-23 13:03:27 +0545

    Add files via upload

---
+++ CommonRoute.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonRoute : MonoBehaviour
+{
+   Transform[] childNodes;
+
+   public List<Transform> childNodeList = new List<Transform>();
+
+   void Start ()
+   {
+       FillNodes();
+   }
+
+   void OnDrawGizmos()
+   {
+       Gizmos.color = Color.green;
+       FillNodes();
+
+       for (int i = 0; i < childNodeList.Count; i++)
+       {
+           Vector3 pos = childNodeList[i].position;
+           if(i>0)
+           {
+               Vector3 prev = childNodeList[i - 1].position;
+               Gizmos.DrawLine(prev, pos);
+           }
+       }
+
+   }
+
+   void FillNodes()
+   {
+       childNodeList.Clear();
+       childNodes = GetComponentsInChildren<Transform>();
+
+       foreach(Transform child in childNodes)
+       {
+           if(child != this.transform)
+           {
+               childNodeList.Add(child);
+           }
+       }
+   }
+}
